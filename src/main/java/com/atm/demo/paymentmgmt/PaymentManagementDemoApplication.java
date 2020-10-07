76fc6e11979622ae07403d9560d7638b32ab6973
DEV-HANDSON <job4pjs@gmail.com>
2020-10-07 14:31:56 -0500

    Initial checkings

---
+++ src/main/java/com/atm/demo/paymentmgmt/PaymentManagementDemoApplication.java
@@ -0,0 +1,13 @@
+package com.atm.demo.paymentmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentManagementDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentManagementDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atm/demo/paymentmgmt/PaymentManagementDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atm.demo.paymentmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentManagementDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
